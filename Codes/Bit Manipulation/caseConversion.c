c7a2cf3dbaa408475ec006c7be5fac40d9c84c47
NidhiMehata <nidhimehata417@gmail.com>
2019-02-15 11:49:54 +0530

    Commit

---
+++ Codes/Bit Manipulation/caseConversion.c
@@ -0,0 +1,16 @@
+/* 
+Case Conversion using bit manipulation 
+    To convert from upper to lower : 
+        - upper or with 32
+    To convert from lower to upper :
+        - lower and with not of 32
+*/
+#include<stdio.h>
+int main(){
+    char upper='A';
+    printf("To Lower : %c\n", upper | 32);
+
+    char lower='a';
+    printf("To Upper : %c", lower & ~32 );
+
+}
